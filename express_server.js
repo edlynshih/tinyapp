5ebdb57039cbd740f2b3a78d38739bba647b535c
Edlyn Shih <edlyn0418@gmail.com>
2022-09-24 20:10:14 -0400

    Create urlsForUser function and add premission features to the app.

--- express_server.js
+++ express_server.js
@@ -16,8 +16,14 @@ app.use(cookieParser());
 // -------------------- FEED DATA -------------------- //
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
 };
 
 const users = {
@@ -40,10 +46,16 @@ app.get("/urls", (req, res) => {
   const userid = req.cookies["user_id"];
   const user = users[userid];
   const templateVars = {
-    urls: urlDatabase,
+    urls: urlsForUser(urlDatabase, userid),
     user
   };
-  res.render("urls_index", templateVars); //res.render takes name of template and an obj, so we can use the key of that obj (urls) to access the data within our template
+
+  if (!userid) { //not sure if this is correct
+    return res.send("Please log in or register to create tinyURL.");
+  } else {
+    res.render("urls_index", templateVars); //res.render takes name of template and an obj, so we can use the key of that obj (urls) to access the data within our template
+  };
+
 });
 
 // get route to show user the form to submit new URLs to be shortened
@@ -51,7 +63,13 @@ app.get("/urls/new", (req, res) => {
   const userid = req.cookies["user_id"];
   const user = users[userid];
   const templateVars = { user };
-  res.render("urls_new", templateVars);
+  
+  if (!templateVars.user) {
+    res.redirect("/login");
+  } else {
+    res.render("urls_new", templateVars);
+  };
+
 });
 
 //Use the id from the route parameter to lookup it's associated longURL from the urlDatabase
@@ -60,17 +78,31 @@ app.get("/urls/:id", (req, res) => { //: infront of the id indicates that id the
   const user = users[userid];
   const templateVars = {
     id: req.params.id,
-    longURL: urlDatabase[req.params.id],
+    longURL: urlDatabase[req.params.id].longURL,
     user
   };
-  res.render("urls_show", templateVars);
+
+  if (!userid) {
+    res.send("Please log in to see your tinyURL!");
+  } else if (userid !== urlDatabase[req.params.id].userID) {
+    res.send("You are not authorized to access this tinyURL.")
+  } else {
+    res.render("urls_show", templateVars);
+  };
+
 });
 
 //requests to the endpoint "/u/:id" will redirect to its longURL
 app.get("/u/:id", (req, res) => {
   const id = req.params.id;
-  const longURL = urlDatabase[id];
+  const longURL = urlDatabase[id].longURL;
+  
+  if (!urlDatabase[id]) {
+    return res.status(404).send("This tiny URL does not exist.");
+  } 
+
   res.redirect(longURL);
+
 });
 
 //get route to show user registration form
@@ -78,42 +110,82 @@ app.get("/register", (req, res) => {
   const userid = req.cookies["user_id"];
   const user = users[userid];
   const templateVars = { user };
-  res.render("urls_registration", templateVars);
-})
+
+  if (templateVars.user) {
+    res.redirect("/urls");
+  } else {
+    res.render("urls_registration", templateVars);
+  };
+  
+});
 
 //get route to show user login form
 app.get("/login", (req, res) => {
   const userid = req.cookies["user_id"];
   const user = users[userid];
   const templateVars = { user };
+
   if (templateVars.user) {
     res.redirect("/urls");
   } else {
     res.render("urls_login", templateVars);
   };
+
 });
 
 //post route to handle the form submission
 app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();
-  const longURL = req.body.longURL; //post req has a body, data in the input field is avaialble in the req.body.longURL variable (can store in the urlDatabase obj)
-  urlDatabase[shortURL] = longURL; //save longURL and shortURL id to our urlDatabase
-  res.redirect(`/urls/${shortURL}`); //redirect the user to a new page that shows the short url. Browser makes a GET request to /urls/:id (urls_show)
+
+  if (!req.cookies["user_id"]) { //not sure if this is correct
+    res.send("Please log in to create new URL.");
+  } else {
+    const shortURL = generateRandomString();
+    const longURL = req.body.longURL; //post req has a body, data in the input field is avaialble in the req.body.longURL variable (can store in the urlDatabase obj)
+    const userID = req.cookies["user_id"];
+    urlDatabase[shortURL] = { 
+      longURL: longURL,
+      userID: userID
+    }; //save longURL and userID keys to our urlDatabase
+    res.redirect(`/urls/${shortURL}`); //redirect the user to a new page that shows the short url. Browser makes a GET request to /urls/:id (urls_show)
+  };
+
 });
 
 //post route that removes a URL resource
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
-  delete urlDatabase[id];
-  res.redirect("/urls");
+  const userid = req.cookies["user_id"];
+  
+  if (!urlDatabase[id]) {
+    res.send("This tiny URL does not exist.");
+  } else if (!userid) {
+    res.send("Please log in to delete your tinyURL!");
+  } else if (userid !== urlDatabase[req.params.id].userID) {
+    res.send("You are not authorized to delete this tinyURL.");
+  } else {
+    delete urlDatabase[id];
+    res.redirect("/urls");
+  };
+
 });
 
 //post route to update the value of the stored long URL based on the new value in req.body
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = req.body.longURL;
-  urlDatabase[id] = longURL;
-  res.redirect("/urls");
+  urlDatabase[id].longURL = longURL;
+  const userid = req.cookies["user_id"];
+
+  if (!userid) {
+    res.send("Please log in to see your tinyURL!");
+  } else if (!urlDatabase[id]) {
+    res.send("This tiny URL does not exist.");
+  } else if (userid !== urlDatabase[req.params.id].userID) {
+    res.send("You are not authorized to access this tinyURL.");
+  } else {
+    res.redirect("/urls");
+  };
+  
 });
 
 //post route to handle login and set a cookie
@@ -125,10 +197,13 @@ app.post("/login", (req, res) => {
     return res.status(403).send("The email cannot be found.");
   } else if (user.password !== password) {
     return res.status(403).send("Incorrect credentials. Try again.");
+  } else if (!email || !password) {
+    return res.status(400).send("The email or password cannot be blank. Please try again.");
   } else {
     res.cookie("user_id", user.id)
     res.redirect("/urls");
   };
+
 });
 
 //post route to handle logout and clear cookie
@@ -155,6 +230,7 @@ app.post("/register", (req, res) => {
     res.cookie("user_id", users[uniqueID].id);
     res.redirect("/urls");
   }
+
 });
 
 app.listen(PORT, () => {
@@ -170,4 +246,15 @@ function generateRandomString() {
 //return user obj with matching email
 function getUserByEmail(email, users) {
   return Object.values(users).find(user => user.email === email);
+};
+
+//return the URLs where the userID is equal to the id of the currently logged in user
+function urlsForUser(urlDatabase, userID) {
+  const filteredURLS = {};
+  for (let shortURL in urlDatabase) {
+    if (userID === urlDatabase[shortURL].userID) {
+      filteredURLS[shortURL] = urlDatabase[shortURL];
+    }
+  };
+  return filteredURLS;
 };
